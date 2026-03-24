a122789494978511ca612f8026f35e6ba67240ac
anuki alwis <anuki.alwis@fmsi.com>
2026-03-24 12:17:08 +0530

    Reducing the number of browsers running the same testcase. (because: it drains the available time slots as follows.) If one user(staff) is available, then the customer can add an appointment to that person. But if the appointment is 60min long and the appointment time was 10.00AM, that staff will NOT be available from 10.00 - 11.00. Which is ( 10.00AM, 10.15AM, 10.30AM, 10.45AM, 11.00AM timeslots will be affected by that 1 appointment) Therefore, keeping Chrome brower and removing firefox, webKit browser running instances.

--- playwright.config.ts
+++ playwright.config.ts
@@ -58,17 +58,16 @@ export default defineConfig({
     {
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
-    },
+    },  
+    // {
+    //   name: 'firefox',
+    //   use: { ...devices['Desktop Firefox'] },
+    // },
     
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
-    
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
+    // {
+    //   name: 'webkit',
+    //   use: { ...devices['Desktop Safari'] },
+    // },
   ],
 
   /* Global setup/teardown for authentication */
